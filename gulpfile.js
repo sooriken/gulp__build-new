5f4894e34d2c8f5c2ebe56b1b73ddbf63a6c5276
Курганов Никита <110167907+sooriken@users.noreply.github.com>
2024-01-26 20:13:56 +0300

    Js update
    Обновление скрипт файлов

--- gulpfile.js
+++ gulpfile.js
@@ -11,28 +11,46 @@ const clean        = require('gulp-clean');
 const fs           = require('fs');
 const concat       = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
-const minCss       = require('gulp-minify-css')
-const rename       = require('gulp-rename')
-const sourceMaps   = require('gulp-sourcemaps')
-const plumber      = require('gulp-plumber')
-const notify      = require('gulp-notify')
+const minCss       = require('gulp-minify-css');
+const rename       = require('gulp-rename');
+const sourceMaps   = require('gulp-sourcemaps');
+const plumber      = require('gulp-plumber');
+const notify      = require('gulp-notify');
+
+const webpack = require('webpack-stream');
 // открыть при финальной сборке (пакет группировки медиа-запросов, который ломает сурс-мапс):
 // const groupMedia   = require('gulp-group-css-media-queries')
 
 // конфиг файлы
-var config = {
-    srcCss   : './app/src/sass/*.sass',
-    srcPug   : './app/index.pug',
-    destCss  : './app/css',
-    destCssMin  : './app/css-min',
-    srcImage : './app/src/images/**/*',
-    destImage: 'dist/images',
-    anySass  : './app/src/sass/**/*.sass',
-    anyPug   : './app/**/*.pug',
-    srcFonts : './app/fonts/**/*',
-    destFonts : './dist/fonts',
-    srcFiles : './app/files/**/*',
-    destFiles : './dist/files'
+var sources = {
+    // css
+    css          : './app/src/sass/*.sass',
+    destCss      : './app/css',
+    // css min
+    cssMin       : './app/css-min/**/*',
+    destCssMin   : './app/css-min',
+    // sass
+    anySass      : './app/src/sass/**/*.sass',
+    // pug
+    pug          : './app/index.pug',
+    anyPug       : './app/pages/*.pug',
+    // html
+    html         : './app/**/*.html',
+    // image
+    image        : './app/src/images/**/*',
+    destImage    : 'dist/images',
+    // fonts
+    fonts        : './app/fonts/**/*',
+    destFonts    : './dist/fonts',
+    // files
+    files        : './app/files/**/*',
+    destFiles    : './dist/files',
+    // js
+    js           : './app/src/js/*.js',
+    destJs       : './app/src/js-bundle',
+    anyJs        : './app/src/js/**/*.js',
+    // webpack config
+    webpackConfig: './webpack.config.js',
 }
 
 // настройки сервера
@@ -52,20 +70,25 @@ const plumberNotify = (title) => {
     };
 };
 
-// функция pug (преобразует index.pug файл)
+// функция pug (преобразует index.pug)
 gulp.task('pug', function() {
-    return gulp
-        .src(config.srcPug)
+    return src(sources.pug)
         .pipe(plumber(plumberNotify('Pug')))
         .pipe(pug({pretty: true}))
         .pipe(dest('./app/'))
-//        .pipe(browserSync.stream())
+});
+
+// функция anyPug (преобразует все pug файлы)
+gulp.task('anyPug', function() {
+    return src(sources.anyPug)
+        .pipe(plumber(plumberNotify('Pug')))
+        .pipe(pug({pretty: true}))
+        .pipe(dest('./app/'))
 });
 
 // функция sass (конвертирует все sass файлы)
 gulp.task('sass', function() {
-    return gulp
-        .src(config.srcCss)
+    return src(sources.css)
         .pipe(plumber(plumberNotify('Sass')))
         .pipe(sourceMaps.init())
 //        вывод экспандед версии css файла:
@@ -76,32 +99,41 @@ gulp.task('sass', function() {
         .pipe(sourceMaps.write())
 //        следующий пакет ломает sourceMaps, но группирует медиа-запросы:
 //        .pipe(groupMedia())
-        .pipe(dest(config.destCss))
+        .pipe(dest(sources.destCss))
         // вывод минифицированной версии css файла:
         .pipe(minCss())
         .pipe(rename({ extname: '.min.css' }))
         .pipe(sourceMaps.write())
-        .pipe(dest(config.destCssMin))
-})
+        .pipe(dest(sources.destCssMin))
+});
 
+// функция js webpack
+gulp.task('js', function() {
+    return src(sources.js)
+        .pipe(plumber(plumberNotify('Js')))
+        .pipe(webpack(require(sources.webpackConfig)))
+        .pipe(dest(sources.destJs));
+})
 
 // функция лайв-сервера
 gulp.task('server', function() {
-	return gulp.src('./app/')
+	return src('./app/')
 		.pipe(server(serverOptions));
 });
 
 // функция watch (слежение за sass и pug файлами)
 gulp.task('watch', function() {
-	gulp.watch(config.anySass, gulp.parallel('sass'));
-	gulp.watch(config.anyPug, gulp.parallel('pug'));
-//    gulp.watch('./src/**/*', gulp.parallel('images));
+	gulp.watch(sources.anySass, gulp.parallel('sass'));
+	gulp.watch(sources.pug, gulp.parallel('pug'));
+	gulp.watch(sources.anyJs, gulp.parallel('js'));
+    // при разработке многостраничного сайта (подключает pug файлы из директории pages):
+	gulp.watch(sources.anyPug, gulp.parallel('anyPug'));
 });
 
 // функция clean (очищает папку "dist")
 gulp.task('clean', function(done) {
     if (fs.existsSync('./dist/')) {
-	   return gulp.src('./dist/', {read: false})
+	   return src('./dist/', {read: false})
            .pipe(clean({ force: true }));
     }
     done();
@@ -109,13 +141,13 @@ gulp.task('clean', function(done) {
 
 // функция default (clean, pug, sass, server, watch)
 gulp.task('default', gulp.series(
-    gulp.parallel('pug', 'sass'),
+    gulp.parallel('pug', 'sass', 'js'),
     gulp.parallel('server', 'watch')
 ));
 
 //функция сжатия изображений
 gulp.task('images', function() {
-    return src(config.srcImage)
+    return src(sources.image)
     .pipe(imagemin([
         imagemin.gifsicle({interlaced: true}),
         imagemin.mozjpeg({quality: 75, progressive: true}),
@@ -128,34 +160,30 @@ gulp.task('images', function() {
             })
         ]
     ))
-    .pipe(dest(config.destImage))
+    .pipe(dest(sources.destImage))
 });
 
 // функция копирования шрифтов в готовую папку dist
 gulp.task('fonts', function() {
-    return gulp
-        .src(config.srcFonts)
-        .pipe(dest(config.destFonts));
+    return src(sources.fonts)
+        .pipe(dest(sources.destFonts));
 });
 
 // функция копирования файлов в готовую папку dist
 gulp.task('files', function() {
-    return gulp
-        .src(config.srcFiles)
-        .pipe(dest(config.destFiles));
+    return src(sources.files)
+        .pipe(dest(sources.destFiles));
 });
 
 // функция копирования index.html в готовую папку dist
 gulp.task('html', function() {
-    return gulp
-        .src('./app/index.html')
+    return src(sources.html)
         .pipe(dest('./dist'));
 });
 
 // функция копирования css файлов в готовую папку dist
 gulp.task('css', function() {
-    return gulp
-        .src('./app/css-min/**/*')
+    return src(sources.cssMin)
         .pipe(dest('./dist/css-min'));
 });
 
