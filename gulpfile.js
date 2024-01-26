da6ae8bc7c7d88c68a6dab051aedfcbe95c52fb2
Курганов Никита <110167907+sooriken@users.noreply.github.com>
2024-01-26 08:51:13 +0300

    First commit
    Первая версия "неотлаженной" сборки

---
+++ gulpfile.js
@@ -0,0 +1,145 @@
+//дефолтные переменные gulp
+const { src, dest, watch, parallel, series } = require('gulp');
+
+//объявление переменных пакетов
+const gulp         = require('gulp');
+const pug          = require('gulp-pug');
+const sass         = require('gulp-sass')(require('sass'));
+const imagemin     = require('gulp-imagemin');
+const server       = require('gulp-server-livereload');
+const clean        = require('gulp-clean');
+const fs           = require('fs');
+const concat       = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const minCss       = require('gulp-minify-css')
+const rename       = require('gulp-rename')
+const sourceMaps   = require('gulp-sourcemaps')
+//const groupMedia   = require('gulp-group-css-media-queries')
+const plumber      = require('gulp-plumber')
+const notify      = require('gulp-notify')
+
+// конфиг файлы
+var config = {
+    srcCss   : './app/src/sass/*.sass',
+    srcPug   : './app/index.pug',
+    destCss  : './app/css',
+    srcImage : './app/src/images/**/*',
+    destImage: 'dist/images',
+    anySass  : './app/src/sass/**/*.sass',
+    anyPug   : './app/**/*.pug',
+    srcFonts : './app/fonts/**/*',
+    destFonts : './dist/fonts',
+    srcFiles : './app/files/**/*',
+    destFiles : './dist/files'
+}
+
+// настройки сервера
+const serverOptions = {
+			livereload: true,
+			open: true
+	}
+// функция настроек plumber
+const plumberNotify = (title) => {
+    return {
+        errorHandler: notify.onError({
+            title: title,
+            message: 'Error <%= error.message %>',
+            sound: false
+    }),
+    };
+};
+
+// функция pug (преобразует index.pug файл)
+gulp.task('pug', function() {
+    return gulp
+        .src(config.srcPug)
+        .pipe(plumber(plumberNotify('Pug')))
+        .pipe(pug({pretty: true}))
+        .pipe(dest('./app/'))
+//        .pipe(browserSync.stream())
+});
+
+// функция sass (конвертирует все sass файлы)
+gulp.task('sass', function() {
+    return gulp
+        .src(config.srcCss)
+        .pipe(plumber(plumberNotify('Sass')))
+        .pipe(sourceMaps.init())
+//        вывод экспандед версии css файла
+        .pipe(sass({
+            outputStyle : 'expanded'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourceMaps.write())
+//        следующий пакет ломает sourceMaps, но группирует медиа-запросы:
+//        .pipe(groupMedia())
+        .pipe(dest(config.destCss))
+        // вывод минифицированной версии css файла
+        .pipe(minCss())
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(sourceMaps.write())
+        .pipe(dest(config.destCss))
+//        .pipe(browserSync.stream())
+})
+
+//функция сжатия изображений
+gulp.task('images', function() {
+    return src(config.srcImage)
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.mozjpeg({quality: 75, progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+                ]
+            })
+        ]
+    ))
+    .pipe(dest(config.destImage))
+});
+
+// функция копирования шрифтов в готовую папку dist
+gulp.task('fonts', function() {
+    return gulp
+        .src(config.srcFonts)
+        .pipe(gulp.dest(config.destFonts));
+});
+
+// функция копирования файлов в готовую папку dist
+gulp.task('files', function() {
+    return gulp
+        .src(config.srcFiles)
+        .pipe(gulp.dest(config.destFiles));
+});
+
+// функция лайв-сервера
+gulp.task('server', function() {
+	return gulp.src('./app/')
+		.pipe(server(serverOptions));
+});
+
+// функция watch (слежение за sass и pug файлами)
+gulp.task('watch', function() {
+	gulp.watch(config.anySass, gulp.parallel('sass'));
+	gulp.watch(config.anyPug, gulp.parallel('pug'));
+//    gulp.watch('./src/**/*', gulp.parallel('images));
+});
+
+// функция clean (очищает папку "dist")
+gulp.task('clean', function(done) {
+	if (fs.existsSync('./dist/')){
+	return gulp.src('./dist/'), { read: false }
+		.pipe(clean({ force: true }));
+	}
+	done();
+});
+
+// функция default (clean, pug, sass, images, server, watch)
+gulp.task('default', gulp.series(
+    gulp.parallel('pug', 'sass'),
+    gulp.parallel('server', 'watch')
+));
+
+// функция build
